0535a33126d4f8d67cff005d181254faac570512
nxhvhnn <104713671+nxhvhnn@users.noreply.github.com>
2024-03-12 08:07:14 +0100

    Update dhcp.js

--- docs/ui/dhcp.js
+++ docs/ui/dhcp.js
@@ -19,45 +19,22 @@ export default {
   uuid: {
     eth: 0xFF01,
     dhcp: 0xFF05,
+    dns: 0xFF06,
   },
   update: (c)=>{
-  const lable = lables[c.value.eth.getUint8(3)];
+  const lable = lables[c.value.eth.getUint8(3)] ;
   const okness = oknesses[lable];
 
-
-  const val1 = c.value.dhcp.getUint8(0);
-  const val2 = c.value.dhcp.getUint8(1);
-  const val3 = c.value.dhcp.getUint8(2);
-  const val4 = c.value.dhcp.getUint8(3);
-
-  const val5 = c.value.dhcp.getUint8(4);
-  const val6 = c.value.dhcp.getUint8(5);
-  const val7 = c.value.dhcp.getUint8(6);
-  const val8 = c.value.dhcp.getUint8(7);
-
-  const val9 = c.value.dhcp.getUint8(8);
-  const val10 = c.value.dhcp.getUint8(9);
-  const val11 = c.value.dhcp.getUint8(10);
-  const val12 = c.value.dhcp.getUint8(11);
-
-  const val13 = c.value.dhcp.getUint8(12);
-  const val14 = c.value.dhcp.getUint8(13);
-  const val15 = c.value.dhcp.getUint8(14);
-  const val16 = c.value.dhcp.getUint8(15);
-
-  const val17 = c.value.dhcp.getUint8(16);
-  const val18 = c.value.dhcp.getUint8(17);
-  const val19 = c.value.dhcp.getUint8(18);
-  const val20 = c.value.dhcp.getUint8(19);
-
+  const val = c.value.dhcp.getUint8(0);
   
-  const device_ip = `${val1}.${val2}.${val3}.${val4}`;
-  const mask_ip = `${val5}.${val6}.${val7}.${val8}`;
-  const gateway = `${val9}.${val10}.${val11}.${val12}`;
-  const main_dns = `${val13}.${val14}.${val15}.${val16}`;
-  const sec_dns = `${val17}.${val18}.${val19}.${val20}`;
+  const device_ip = `${c.value.dhcp.getUint8(0)}.${c.value.dhcp.getUint8(1)}.${c.value.dhcp.getUint8(2)}.${c.value.dhcp.getUint8(3)}`;
+  const mask_ip = `${c.value.dhcp.getUint8(4)}.${c.value.dhcp.getUint8(5)}.${c.value.dhcp.getUint8(6)}.${c.value.dhcp.getUint8(7)}`;
+  const gateway = `${c.value.dhcp.getUint8(8)}.${c.value.dhcp.getUint8(9)}.${c.value.dhcp.getUint8(10)}.${c.value.dhcp.getUint8(11)}`;
+  
+  const main_dns = `${c.value.dns.getUint8(0)}.${c.value.dns.getUint8(1)}.${c.value.dns.getUint8(2)}.${c.value.dns.getUint8(3)}`;
+  const sec_dns = `${c.value.dns.getUint8(4)}.${c.value.dns.getUint8(5)}.${c.value.dns.getUint8(6)}.${c.value.dns.getUint8(7)}`;
 
-  if (val1 != 0) {
+  if (val != 0) {
     return html`
     <div class="tile ${device_ip?'good' :'bad'}">
       <span class="title">DHCP</span>
