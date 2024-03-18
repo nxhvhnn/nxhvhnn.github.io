b55f0674adacb9f2138a62e0b2fbd6501831032a
nxhvhnn <104713671+nxhvhnn@users.noreply.github.com>
2024-03-18 09:47:35 +0100

    Update lldp.js

--- docs/ui/lldp.js
+++ docs/ui/lldp.js
@@ -1,17 +1,10 @@
 import {html} from 'https://unpkg.com/lit-html@1.4.1?module';
 import tile from './tile.js';
 import {parseLLDP} from '../lldp.js';
-import {parseVLAN} from '../vlan.js';
 
 function extendObj(base, key, ext) {
   if (!base[key]) base[key] = {};
   base[key] = {...base[key], ...ext};
-}
-
-function getVLANState(vlan) {
-  if (vlan.announced && vlan.detected && vlan.traffic?.arp) return 'good';
-  if (vlan.announced) return 'ok';
-  return 'unknown';
 }
 
 export default {
@@ -22,10 +15,6 @@ export default {
   update: (c)=>{
     const lldp = parseLLDP(c.value.lldp);
     const lldpStatus = c.value.lldp && c.value.lldp.byteLength ? lldp.length ? 'good' : c.value.lldp.byteLength > 2 ? 'bad' : 'ok' : 'unknown';
-    const vlanStatus = c.value.vlan && c.value.vlan.byteLength ? !(c.value.vlan.byteLength % 3) ? 'good' : 'bad' : 'unknown';
-    const detectedVlans = vlanStatus === 'good' ?
-      parseVLAN(c.value.vlan)
-    : [];
 
     const cleanValues = {
       'System name': lldp.find(v=>v.name==='System name')?.value,
@@ -36,42 +25,14 @@ export default {
         v.name==='Vendor Specific'
         && v.value.subtypeName === 'Extended Power-via-MDI'
       )),
-      'Port VLAN ID': lldp.find(v=>
-        v.name==='Vendor Specific'
-        && v.value.subtypeName === 'Port VLAN ID'
-      )?.value?.value,
-      'Network Policy Voice': lldp.find(v=>
-        v.name==='Vendor Specific'
-        && v.value.subtypeName === 'Network Policy'
-        && v.value.value['Application Type']==='Voice'
-      ),
-      'VLAN Name': lldp.filter(v=>
-        v.name==='Vendor Specific'
-        && v.value.subtypeName === 'VLAN Name'
-      )
     }
-
+const managementAddress = cleanValues['Management address']?.['Address']?.['IPv4'];
     const tiles = [
       tile('Switch', cleanValues['System name'] || cleanValues['Chassis ID'], 'ok', cleanValues['Chassis ID']),
+      tile('Management Address', managementAddress, 'ok', cleanValues['Management address']),
       tile('Port', cleanValues['Port description'] || cleanValues['Port ID'], 'ok', cleanValues['Port ID']),
       tile('POE', cleanValues['Extended Power-via-MDI']?.value?.value?.['Power value'], 'good', cleanValues['Extended Power-via-MDI'])
     ];
-
-    const vlans = {};
-
-    const portVLAN = cleanValues['Port VLAN ID'];
-    if (portVLAN) extendObj(vlans, portVLAN, {announced: true, role: 'Port'})
-
-    const voiceVLAN = cleanValues['Network Policy Voice']?.value?.value?.['VLAN ID'];
-    if (voiceVLAN) extendObj(vlans, voiceVLAN, {announced: true, role: 'Voice'})
-
-    cleanValues['VLAN Name'].forEach(v=>
-      extendObj(vlans, v.value.value.id, {name: v.value.value.name, announced: true})
-    );
-
-    detectedVlans.filter(v=>v.id!==0).forEach(v=>extendObj(vlans, v.id, {detected: true, tagged: true, traffic: v.traffic}));
-    if (detectedVlans[0]?.id===0 && detectedVlans[0]?.traffic?.arp)
-      extendObj(vlans, portVLAN || 0, {detected: true, untagged: true, traffic: detectedVlans[0].traffic});
 
     const detail = lldp.map(tlv=>`${tlv.name}: ${JSON.stringify(tlv.value, null, 2)}`).join('\n');
 
